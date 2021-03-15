096ed8b801cdd060cdb86ab704aca5f69543fe95
Farida Alin <farida.alin@yahoo.com>
2021-03-15 21:26:21 +0100

    testing pie chart

--- src/components/country/Country.js
+++ src/components/country/Country.js
@@ -1,30 +1,54 @@
 import React from 'react';
-import { Chart } from 'react-google-charts';
+import { Doughnut } from 'react-chartjs-2';
 
 const Country = ({ country }) => {
-  console.table('country', country);
-  console.table('country.country', country.country);
+  console.table(country);
+
   return (
-    <Chart
-      width={'500px'}
-      height={'300px'}
-      chartType='PieChart'
-      loader={<div>Loading Chart</div>}
-      data={[
-        ['Covi-19 data', 'Percentage'],
-        ['Infected', country.cases],
-        ['Deaths', country.deaths],
-        ['Recovered', country.recovered],
-        ['Active', country.active],
-        ['Infected Today', country.todayCases],
-      ]}
-      options={{
-        title: 'Covid-19 data',
-        // Just add this option
-        is3D: true,
-        pieSliceText: 'value',
+    <Doughnut
+      data={{
+        labels: ['Cases', 'Recovered', 'Deaths', 'Active'],
+        datasets: [
+          {
+            label: '# of votes',
+            data: [
+              country.cases,
+              country.recovered,
+              country.deaths,
+              country.active,
+            ],
+            backgroundColor: [
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              // 'rgba(153, 102, 255, 0.2)',
+              // 'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 206, 86, 1)',
+              'rgba(54, 162, 235, 1)',
+
+              'rgba(255, 99, 132, 1)',
+              'rgba(75, 192, 192, 1)',
+              // 'rgba(153, 102, 255, 1)',
+              // 'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
       }}
-      rootProps={{ 'data-testid': '2' }}
+      height={400}
+      width={400}
+      options={{
+        animation: {
+          animateScale: true,
+        },
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{ ticks: { beginAtZero: true } }],
+        },
+      }}
     />
   );
 };
